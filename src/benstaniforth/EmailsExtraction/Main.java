05d6b8d5f33af29c4b4cbcd27413ec3d0c851dca
Ben Staniforth <benstaniforth@Bens-MacBook-Pro.local>
2019-03-08 13:40:45 +0000

    Counting emails in text

--- src/benstaniforth/EmailsExtraction/Main.java
+++ src/benstaniforth/EmailsExtraction/Main.java
@@ -1,9 +1,25 @@
 package benstaniforth.EmailsExtraction;
 
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Main {
 
-    public static void main(String[] args) {
-	
+    public static void main(String[] args) throws IOException {
+
+        String sampleText = new String(Files.readAllBytes(Paths.get("sample.txt")));
+
+        int counter = 0;
+
+        for (int i = 0; i < sampleText.length(); i++) {
+            if (sampleText.substring(i).startsWith("@softwire.com")){
+                counter++;
+            }
+        }
+
+        System.out.println(counter);
 
     }
 }
