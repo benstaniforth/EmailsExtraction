e317734289680f4eb5d98b893a6a5a0e18ca1b0d
Ben Staniforth <benstaniforth@Bens-MacBook-Pro.local>
2019-03-08 18:20:57 +0000

    Added Hashmap and regex to check for valid emails of all domains

--- src/benstaniforth/EmailsExtraction/Main.java
+++ src/benstaniforth/EmailsExtraction/Main.java
@@ -4,6 +4,7 @@ package benstaniforth.EmailsExtraction;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -15,19 +16,58 @@ public class Main {
 
         String[] words = textFile.split("\\s");
 
-        Pattern pattern = Pattern.compile("softwire\\.com");
+        Pattern pattern = Pattern.compile("^.+@(.+\\..+)$");
 
+        HashMap<String, Integer> myHashMap = new HashMap<>();
 
         int counter = 0;
 
         for (String snippet: words) {
             Matcher matcher = pattern.matcher(snippet);
-            if (matcher.find()){
+
+            if (matcher.find()) {
+                String domain = matcher.group(1);
                 counter++;
+
+                if (domain == null) {
+                    myHashMap.put(domain, counter);
+                } else {
+                    myHashMap.merge(domain, 1, Integer::sum);
+                }
+
             }
         }
 
-        System.out.println(counter);
+        for (String domain : myHashMap.keySet()){
+            System.out.println(domain + " : " + myHashMap.get(domain));
+        }
+
+
 
     }
+
+
 }
+
+// increase the counter for each domain in the hashmap
+//
+//does not exist - put 1
+//does exist - increase value by 1
+
+
+
+//    public static Map myHashMap (String [] words, Pattern pattern) {
+//
+//        HashMap<String, Integer> myHashMap = new HashMap<>();
+//
+//        int counter = 0;
+//
+//        for (String snippet: words) {
+//            Matcher matcher = pattern.matcher(snippet);
+//            if (matcher.find()) {
+//                String domain = matcher.group(1);
+//                counter++;
+//            }
+//        }
+//
+//    }
