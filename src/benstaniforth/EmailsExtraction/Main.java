66cd23630fead6a886e4afb2b39237bedc672341
Ben Staniforth <benstaniforth@Bens-MacBook-Pro.local>
2019-03-08 15:47:50 +0000

    Added pattern and matcher

--- src/benstaniforth/EmailsExtraction/Main.java
+++ src/benstaniforth/EmailsExtraction/Main.java
@@ -4,17 +4,25 @@ package benstaniforth.EmailsExtraction;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        String sampleText = new String(Files.readAllBytes(Paths.get("sample.txt")));
+        String textFile = new String(Files.readAllBytes(Paths.get("sample.txt")));
+
+        String[] words = textFile.split("\\s");
+
+        Pattern pattern = Pattern.compile("softwire\\.com");
+
 
         int counter = 0;
 
-        for (int i = 0; i < sampleText.length(); i++) {
-            if (sampleText.substring(i).startsWith("@softwire.com")){
+        for (String snippet: words) {
+            Matcher matcher = pattern.matcher(snippet);
+            if (matcher.find()){
                 counter++;
             }
         }
